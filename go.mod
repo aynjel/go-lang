a3f0c2ae7f9ae4b88c16d1506199bc0d54d21101
anggi <ortegacanillo76@gmail.com>
2024-01-31 17:07:44 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module booking-app
+module tutorial
 
 go 1.21.6
---
+++ struct.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+func mainStruct() {
+	myBill := newBill("John's bill")
+
+	fmt.Println(myBill)
+}
+
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+
+	return b
+}
